1cd5d8263eeb278e3ed19f5ef4b62a6b4dc5001d
Darrion Whitfield <dwhitfield@ramfield.net>
2024-09-13 16:36:36 -0500

    CHORE: typing

--- src/common/lib/network.rs
+++ src/common/lib/network.rs
@@ -2,7 +2,7 @@ use std::net::IpAddr;
 
 // Function to get the machine's IP address
 pub fn get_machine_ip() -> Option<IpAddr> {
-    let ifaces = pnet::datalink::interfaces();
+    let ifaces: Vec<pnet::datalink::NetworkInterface> = pnet::datalink::interfaces();
     for iface in ifaces {
         for ip in iface.ips {
             if let IpAddr::V4(ipv4) = ip.ip() {
